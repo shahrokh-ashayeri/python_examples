acfbfb06fe7b1cb2084e68ab61bb9d83dfebf913
Shahrokh Ashayeri <Ashayeri.eng@gmail.com>
2024-03-02 15:39:32 +0330

    Update Lambda File

--- Lambda.py
+++ Lambda.py
@@ -1,5 +1,15 @@
+"""
+    Lambda in Python
+"""
+
+# a sequence for traversing on it
 data = [(3, "a"), (2, "f"), (3, "b"), (1, "e"), (5, "d"), (4, "c")]
 
+# Sort above sequnce based on second value on each tuple
 print(list(sorted(data, key=lambda n: n[1])))
+
+# return elements if first value is greater than 3
 print(list(filter(lambda n: n[0] > 3, data)))
+
+# reaturn a list that consist of values * 2
 print(list(map(lambda n: n[0] * 2, data)))
